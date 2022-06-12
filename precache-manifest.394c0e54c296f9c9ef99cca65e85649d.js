b13329e1a521045e29bce785789e6bd6e0b2dc45
techgoast <techgoast@gmail.com>
2022-06-12 08:53:39 +0200

    Updates

---
+++ precache-manifest.394c0e54c296f9c9ef99cca65e85649d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91319251b1dd4bab557e",
+    "url": "/FinalDash/static/css/main.df8b2067.chunk.css"
+  },
+  {
+    "revision": "91319251b1dd4bab557e",
+    "url": "/FinalDash/static/js/main.91319251.chunk.js"
+  },
+  {
+    "revision": "ce47ab81b35290e2776b",
+    "url": "/FinalDash/static/js/1.ce47ab81.chunk.js"
+  },
+  {
+    "revision": "c0b772953849c5df2805",
+    "url": "/FinalDash/static/js/2.c0b77295.chunk.js"
+  },
+  {
+    "revision": "b12108fcc7dbdb42150f",
+    "url": "/FinalDash/static/js/runtime~main.b12108fc.js"
+  },
+  {
+    "revision": "15973e5baa1096146025d662c482b39d",
+    "url": "/FinalDash/index.html"
+  }
+];
